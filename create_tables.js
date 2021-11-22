feb91af3d0c8ec20a37652dcaa0bd58431090c87
Mathieu Malbrecq <malbrecqmathieu@hotmail.com>
2021-11-22 14:42:31 +0000

    Switch to sequelize

--- create_tables.js
+++ create_tables.js
@@ -1,12 +1,6 @@
-const debug = require('debug')('monprojetdemo:create_database');
-const connection = require('./db_connect').connection;
+const { sequelize } = require('./models/students');
 
-
-connection.query(
-    'create table students (id int auto_increment primary key, firstName varchar(100), lastName varchar(100))',
-    function(err, results, fields) {
-      debug(err);
-      debug(results); // results contains rows returned by server
-      connection.close();
-    }
-);
+(async () => {
+  await sequelize.sync({ force: true });
+  await sequelize.close();
+})();
