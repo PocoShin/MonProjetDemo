bb8e1ed737d598c650a6b4014dd602b350aa6775
Francois ROLAND <froland@froland.be>
2021-12-05 11:29:28 +0000

    Add new sample data.

--- models/import_samples.js
+++ models/import_samples.js
@@ -52,6 +52,9 @@ const {
     lastName: "Roland",
   });
 
+  const adgesec = await EducationUnit.create({
+    name: "Administration, gestion et sécurisation des réseaux",
+  });
   const psgbd = await EducationUnit.create({
     name: "Projet SGBD",
   });
@@ -66,39 +69,100 @@ const {
     shortName: "PSGBD_S1_2122",
     academicYear: "2021-2022",
   });
-  const projweb2_s1_222 = await Class.create({
+  const projweb2_s1_2021 = await Class.create({
+    shortName: "PROJWEB2_S1_2021",
+    academicYear: "2020-2021",
+  });
+  const projweb2_s1_2122 = await Class.create({
     shortName: "PROJWEB2_S1_2122",
     academicYear: "2021-2022",
   });
-  const projint_s1_222 = await Class.create({
+  const projint_s1_2122 = await Class.create({
     shortName: "PROJINT_S1_2122",
     academicYear: "2021-2022",
   });
 
+  const p20200907_am = await TeachingPeriod.create({
+    date: "2020-09-07",
+    beginning: "09:00",
+    end: "12:40",
+  });
+  const p20200914_am = await TeachingPeriod.create({
+    date: "2020-09-14",
+    beginning: "09:00",
+    end: "12:40",
+  });
+  const p20200921_am = await TeachingPeriod.create({
+    date: "2020-09-21",
+    beginning: "09:00",
+    end: "12:40",
+  });
+  const p20200928_am = await TeachingPeriod.create({
+    date: "2020-09-28",
+    beginning: "09:00",
+    end: "12:40",
+  });
   const p20211129_am = await TeachingPeriod.create({
     date: "2021-11-29",
     beginning: "09:00",
-    end: "12:30",
+    end: "12:40",
+  });
+  const p20211206_am = await TeachingPeriod.create({
+    date: "2021-12-06",
+    beginning: "09:00",
+    end: "12:40",
+  });
+  const p20211213_am = await TeachingPeriod.create({
+    date: "2021-12-13",
+    beginning: "09:00",
+    end: "12:40",
   });
   const p20211129_pm = await TeachingPeriod.create({
     date: "2021-11-29",
     beginning: "13:20",
     end: "16:50",
   });
+  const p20211206_pm = await TeachingPeriod.create({
+    date: "2021-12-06",
+    beginning: "13:20",
+    end: "16:50",
+  });
+  const p20211213_pm = await TeachingPeriod.create({
+    date: "2021-12-13",
+    beginning: "13:20",
+    end: "16:50",
+  });
 
   await psgbd_s1_2122.setTeacher(mrsSonneville);
-  await projweb2_s1_222.setTeacher(mrRoland);
-  await projint_s1_222.setTeacher(mrRoland);
+  await projweb2_s1_2021.setTeacher(mrRoland);
+  await projweb2_s1_2122.setTeacher(mrRoland);
+  await projint_s1_2122.setTeacher(mrRoland);
 
   await psgbd_s1_2122.setEducationUnit(psgbd);
-  await projweb2_s1_222.setEducationUnit(projweb);
-  await projint_s1_222.setEducationUnit(projint);
+  await projweb2_s1_2021.setEducationUnit(projweb);
+  await projweb2_s1_2122.setEducationUnit(projweb);
+  await projint_s1_2122.setEducationUnit(projint);
 
-  await projweb2_s1_222.addTeachingPeriod(p20211129_am);
-  await projint_s1_222.addTeachingPeriod(p20211129_pm);
+  await projweb2_s1_2021.addTeachingPeriods([
+    p20200907_am,
+    p20200914_am,
+    p20200921_am,
+    p20200928_am,
+  ]);
+  await projweb2_s1_2122.addTeachingPeriods([
+    p20211129_am,
+    p20211206_am,
+    p20211213_am,
+  ]);
+  await projint_s1_2122.addTeachingPeriods([
+    p20211129_pm,
+    p20211206_pm,
+    p20211213_pm,
+  ]);
 
-  await projweb2_s1_222.addStudents([nadege, michael, jonathan]);
-  await projint_s1_222.addStudents([carole, hatim, thierno]);
+  await projweb2_s1_2021.addStudents([carole, hatim]);
+  await projweb2_s1_2122.addStudents([nadege, michael, jonathan]);
+  await projint_s1_2122.addStudents([carole, hatim, thierno]);
 
   debug("Samples imported.");
 
